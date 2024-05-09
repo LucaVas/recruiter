3f3e6400ec74e9ea4e5ac44a5cb40efadc285f7f
LucaVas <luca.vassos@gmail.com>
2024-05-09 10:08:15 +0300

    Add new client endpoint

--- server/src/main/java/dev/lucavassos/recruiter/modules/client/ClientController.java
+++ server/src/main/java/dev/lucavassos/recruiter/modules/client/ClientController.java
@@ -1,22 +1,20 @@
 package dev.lucavassos.recruiter.modules.client;
 
+import dev.lucavassos.recruiter.modules.client.domain.NewClientRequest;
 import dev.lucavassos.recruiter.modules.client.repository.dto.ClientDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "api/v1")
 public class ClientController {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ClientController.class);
 
     @Autowired
     private ClientService service;
@@ -24,7 +22,14 @@ public class ClientController {
 
     @GetMapping("/clients")
     public ResponseEntity<List<ClientDto>> getAllClients() {
-        LOG.info("Received request for all clients.");
-        return new ResponseEntity<>(service.getAllClients(), HttpStatus.OK);
+        log.info("Received request for all clients.");
+        return new ResponseEntity<>(service.getAllClients(0, 10), HttpStatus.OK);
+    }
+
+    @PostMapping("/clients")
+    public ResponseEntity<ClientDto> addClient(
+            @Valid @RequestBody NewClientRequest request) {
+        log.info("Received request to add client: {}", request);
+        return new ResponseEntity<>(service.addClient(request), HttpStatus.CREATED);
     }
 }
