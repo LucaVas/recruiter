b4cfc32fdcacb801b6129ef992d3b6e0edfb009c
LucaVas <luca.vassos@gmail.com>
2024-05-11 22:16:25 +0300

    Format candidacy documents modal

--- client/src/components/candidacy/files/CandidacyFilesModal.vue
+++ client/src/components/candidacy/files/CandidacyFilesModal.vue
@@ -2,14 +2,18 @@
   <Dialog
     :visible="visible"
     modal
-    header="Candidacy Documents"
+    header="Candidacy documents"
     @update:visible="$emit('close')"
     :style="{ width: '50rem' }"
     :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
   >
     <div class="flex flex-col gap-4">
-      <div v-for="file in files" :key="file.id">
-        {{ file }}
+      <div v-for="file in files" :key="file.id" class="flex w-full items-center">
+        <i class="pi pi-file mr-2" />
+        <div class="flex w-full justify-between">
+          <span>{{ file.name }}</span>
+          <span>{{ formatDateTime(file.createdDTime) }}</span>
+        </div>
       </div>
     </div>
   </Dialog>
@@ -17,6 +21,7 @@
 
 <script setup lang="ts">
 import { type CandidacyFile } from '@/stores/candidacy/schema';
+import { formatDateTime } from '@/utils/dateUtils';
 
 const { visible, files } = defineProps<{
   visible: boolean;
