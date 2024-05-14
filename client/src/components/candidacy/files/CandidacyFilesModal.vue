e9fa95a133800273248d3fe65496a744a993cbd0
LucaVas <luca.vassos@gmail.com>
2024-05-14 19:45:43 +0300

    Add info tooltip & download + delete buttons (31)

--- client/src/components/candidacy/files/CandidacyFilesModal.vue
+++ client/src/components/candidacy/files/CandidacyFilesModal.vue
@@ -11,11 +11,15 @@
       <ProgressSpinner />
     </div>
     <div v-else class="flex flex-col gap-4">
-      <div v-for="file in files" :key="file.id" class="flex w-full items-center">
+      <div v-for="file in files" :key="file.id" class="flex w-full items-center py-4 border-b">
         <i class="pi pi-file mr-2" />
         <div class="flex w-full justify-between">
           <span>{{ file.name }}</span>
-          <span>{{ formatDateTime(file.createdDTime) }}</span>
+          <div class="flex gap-2 items-center">
+            <Button icon="pi pi-download" unstyled @click="$emit('download', file.id)" />
+            <Button icon="pi pi-trash" unstyled @click="$emit('delete', file.id)" />
+            <i class="pi pi-info-circle mr-2" v-tooltip.left="'Uploaded on ' + formatDateTime(file.createdDTime)" />
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +29,7 @@
 <script setup lang="ts">
 import Dialog from 'primevue/dialog';
 import ProgressSpinner from 'primevue/progressspinner';
+import Button from 'primevue/button';
 import { type CandidacyFile } from '@/stores/candidacy/schema';
 import { formatDateTime } from '@/utils/dateUtils';
 
@@ -36,5 +41,7 @@ const { visible, files, loading } = defineProps<{
 
 defineEmits<{
   (e: 'close'): void;
+  (e: 'download', fileId: number): void;
+  (e: 'delete', fileId: number): void;
 }>();
 </script>
