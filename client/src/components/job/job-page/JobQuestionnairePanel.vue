997b94b40157ca6c404459827ae3d59319828be6
LucaVas <luca.vassos@gmail.com>
2024-06-04 16:31:59 +0300

    Add questionnaire panel in job view

---
+++ client/src/components/job/job-page/JobQuestionnairePanel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { type Questionnaire } from '@/stores/questionnaire/schema';
+import Panel from 'primevue/panel';
+
+const { questionnaire } = defineProps<{
+  questionnaire: Questionnaire;
+}>();
+</script>
+
+<template>
+  <Panel class="w-full" header="Questionnaire" toggleable collapsed>
+    <p class="m-0">
+      {{ questionnaire }}
+    </p>
+  </Panel>
+</template>
