6eda2d77a64b130a0cf9f517ea118bb54155db9f
LucaVas <luca.vassos@gmail.com>
2024-06-17 10:50:58 +0300

    Refactor candidate dtos

--- server/src/main/java/dev/lucavassos/recruiter/modules/candidate/repository/dto/CandidateDto.java
+++ server/src/main/java/dev/lucavassos/recruiter/modules/candidate/repository/dto/CandidateDto.java
@@ -1,18 +1,26 @@
 package dev.lucavassos.recruiter.modules.candidate.repository.dto;
 
 import dev.lucavassos.recruiter.modules.candidate.domain.CandidateStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-public record CandidateDto(
-        String pan,
-        String name,
-        String phone,
-        String email,
-        Double totalExperience,
-        String education,
-        Double currentCtc,
-        CandidateStatus status,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-) {}
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public final class CandidateDto {
+    private final String pan;
+    private final String name;
+    private final String phone;
+    private final String email;
+    private final Double totalExperience;
+    private final String education;
+    private final Double currentCtc;
+    private final CandidateStatus status;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+}
